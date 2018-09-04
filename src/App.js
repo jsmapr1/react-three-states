cac56cf5a88c8870d89cee5fa3580b17b77aabc5
Joe Morgan <joe.morgan@bottomline.com>
2018-09-04 14:49:27 -0400

    basic

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <div>Header</div>
+        <article>
+          <section>
+            Promotion
+          </section>
+          <section>
+            <h1>Merchandise</h1>
+            <ul>
+              <li>Item 1</li>
+              <li>Item 2</li>
+              <li>Item 3</li>
+              <li>Item 4</li>
+            </ul>
+          </section>
+        </article>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStroopwafel } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faStroopwafel)
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
